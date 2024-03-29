2a5cc9d06f1927b5c747b00efe548e3de27bf791
kamron <kbariev7@gmail.com>
2024-03-30 03:43:43 +0500

    Spring start

---
+++ src/main/java/uz/kamron/springcource/TestBean.java
@@ -0,0 +1,17 @@
+package uz.kamron.springcource;
+
+public class TestBean {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public TestBean(String name) {
+        this.name = name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
