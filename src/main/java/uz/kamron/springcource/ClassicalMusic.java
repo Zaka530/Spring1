38cd5c8356185ecb578762a27d2a334e34cd2430
kamron <kbariev7@gmail.com>
2024-03-31 03:43:11 +0500

    lesson 10

--- src/main/java/uz/kamron/springcource/ClassicalMusic.java
+++ src/main/java/uz/kamron/springcource/ClassicalMusic.java
@@ -1,17 +1,11 @@
 package uz.kamron.springcource;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class ClassicalMusic implements Music {
 private ClassicalMusic(){};
-    public void doMyInit(){
-        System.out.println("Doing my initialization");
-    }
 
-    public static ClassicalMusic getClassicalMusic(){
-        return new ClassicalMusic();
-    }
-   public void doMyDestroy(){
-       System.out.println("Doing my destruction");
-   }
 
 
     @Override
--- src/main/java/uz/kamron/springcource/TestSpring.java
+++ src/main/java/uz/kamron/springcource/TestSpring.java
@@ -8,22 +8,14 @@ public class    TestSpring {
                 "applicationContext.xml");
 
 
-        ClassicalMusic classicalMusic=context.getBean("musicBean",ClassicalMusic.class);
-        System.out.println(classicalMusic.getSong());
 
+        Music music=context.getBean("rockMusic",Music.class);
+        MusicPlayer musicPlayer=new MusicPlayer(music);
+        musicPlayer.playMusic();
 
-//        MusicPlayer firstMusicPlayer=context.getBean("musicPlayer",MusicPlayer.class);
-//        MusicPlayer secondMusicPlayer=context.getBean("musicPlayer",MusicPlayer.class);
-//
-//        boolean comparison=firstMusicPlayer==secondMusicPlayer;
-//
-//        System.out.println(comparison);
-//        System.out.println(firstMusicPlayer);
-//        System.out.println(secondMusicPlayer);
-//
-//        firstMusicPlayer.setVolume(10);
-//        System.out.println(firstMusicPlayer.getVolume());
-//        System.out.println(secondMusicPlayer.getVolume());
+        Music music2=context.getBean("classicalMusic",Music.class);
+        MusicPlayer classicalMusicPlayer=new MusicPlayer(music2);
+        classicalMusicPlayer.playMusic();
 
         context.close();
     }
