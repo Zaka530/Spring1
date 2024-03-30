4c1bad34ea3ae6ecfa416fa392ac68a5871b2f9c
kamron <kbariev7@gmail.com>
2024-03-30 09:33:17 +0500

    lesson 9

--- src/main/java/uz/kamron/springcource/ClassicalMusic.java
+++ src/main/java/uz/kamron/springcource/ClassicalMusic.java
@@ -1,6 +1,19 @@
 package uz.kamron.springcource;
 
 public class ClassicalMusic implements Music {
+private ClassicalMusic(){};
+    public void doMyInit(){
+        System.out.println("Doing my initialization");
+    }
+
+    public static ClassicalMusic getClassicalMusic(){
+        return new ClassicalMusic();
+    }
+   public void doMyDestroy(){
+       System.out.println("Doing my destruction");
+   }
+
+
     @Override
     public String getSong() {
         return "Chief keef";
