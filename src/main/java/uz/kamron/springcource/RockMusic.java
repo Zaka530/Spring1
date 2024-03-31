d03b200f3d39216869c261d719236c541862f83f
Zaka530 <kbariev7@gmail.com>
2024-03-31 16:11:49 +0500

    lesson 11

--- src/main/java/uz/kamron/springcource/RockMusic.java
+++ src/main/java/uz/kamron/springcource/RockMusic.java
@@ -2,7 +2,7 @@ package uz.kamron.springcource;
 
 import org.springframework.stereotype.Component;
 
-@Component("rockMusic")
+@Component
 public class RockMusic implements Music{
     @Override
     public String getSong() {
--- src/main/java/uz/kamron/springcource/TestSpring.java
+++ src/main/java/uz/kamron/springcource/TestSpring.java
@@ -8,14 +8,21 @@ public class    TestSpring {
                 "applicationContext.xml");
 
 
+         Computer computer=context.getBean("computer",Computer.class);
+        System.out.println(computer);
 
-        Music music=context.getBean("rockMusic",Music.class);
-        MusicPlayer musicPlayer=new MusicPlayer(music);
-        musicPlayer.playMusic();
 
-        Music music2=context.getBean("classicalMusic",Music.class);
-        MusicPlayer classicalMusicPlayer=new MusicPlayer(music2);
-        classicalMusicPlayer.playMusic();
+
+//        MusicPlayer musicPlayer=context.getBean("musicPlayer",MusicPlayer.class);
+//        musicPlayer.playMusic();
+
+//        Music music=context.getBean("rockMusic",Music.class);
+//        MusicPlayer musicPlayer=new MusicPlayer(music);
+//        musicPlayer.playMusic();
+//
+//        Music music2=context.getBean("classicalMusic",Music.class);
+//        MusicPlayer classicalMusicPlayer=new MusicPlayer(music2);
+//        classicalMusicPlayer.playMusic();
 
         context.close();
     }
